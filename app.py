b0313111eefec6750d925addf213b7b31b10f692
heathergi85 <heathergi85@gmail.com>
2020-06-17 09:04:23 +0000

    requirements /  procfile added

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from flask import Flask
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@dictionary-lopfd.mongodb.net/task_manager?retryWrites=true&w=majority'
-
-mongo = PyMongo(app)
-
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
-
+def hello():
+    return 'Hello World ...again'
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
