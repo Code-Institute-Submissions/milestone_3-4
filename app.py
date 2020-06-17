203c8ba36d44b2445b2ea1c684f0740a0b05c36e
heathergi85 <heathergi85@gmail.com>
2020-06-17 17:03:20 +0000

    added addword, app.py updated

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@dictionary-lopfd.mongodb.net/<dbname>?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'dictionary_db'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@dictionary-lopfd.mongodb.net/dictionary_db?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+@app.route('/index')
+def index():
+    return render_template("index.html", diagnosis=mongo.db.diagnosis.find())
 
+@app.route('/add_word')
+def add_word():
+    return render_template('addword.html',
+                           speciality=mongo.db.speciality_name.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
