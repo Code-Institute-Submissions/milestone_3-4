c512f0b2ca505f94f9eb9b9fbdb1421cb85cc367
heathergi85 <heathergi85@gmail.com>
2020-06-18 08:47:08 +0000

    crud specialities added, cards format added

--- app.py
+++ app.py
@@ -40,6 +40,50 @@ def edit_word(terms_id):
                            speciality=all_specialities)
 
 
+@app.route('/update_word/<terms_id>', methods=["POST"])
+def update_word(terms_id):
+    terms = mongo.db.terms
+    terms.update( {'_id': ObjectId(terms_id)},
+    {
+        'term':request.form.get('term'),
+        'definition':request.form.get('definition'),
+        'speciality_name': request.form.get('speciality_name'),
+    })
+    return redirect(url_for('index'))
+
+@app.route('/get_specialities')
+def get_specialities():
+    return render_template('specialities.html',
+                           speciality=mongo.db.speciality.find())
+
+@app.route('/edit_speciality/<speciality_id>')
+def edit_speciality(speciality_id):
+    return render_template('editspeciality.html',
+    speciality=mongo.db.speciality.find_one({'_id': ObjectId(speciality_id)}))
+
+
+@app.route('/update_speciality/<speciality_id>', methods=['POST'])
+def update_speciality(speciality_id):
+    mongo.db.speciality.update(
+        {'_id': ObjectId(speciality_id)},
+        {'speciality_name': request.form.get('speciality_name')})
+    return redirect(url_for('get_specialities'))
+
+@app.route('/insert_speciality', methods=['POST'])
+def insert_speciality():
+    speciality_doc = {'speciality_name': request.form.get('speciality_name')}
+    mongo.db.speciality.insert_one(speciality_doc)
+    return redirect(url_for('get_specialities'))    
+
+@app.route('/delete_speciality/<speciality_id>')
+def delete_speciality(speciality_id):
+    mongo.db.speciality.remove({'_id': ObjectId(speciality_id)})
+    return redirect(url_for('get_specialities'))
+
+@app.route('/add_speciality')
+def add_speciality():
+    return render_template('addspeciality.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
