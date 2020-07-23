69944e9a6f1f4e1693ba50c4a1bc8db1f04a86d5
heathergi85 <heathergi85@gmail.com>
2020-07-23 19:49:41 +0000

    filter function added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
-import re
+
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'dictionary_db'
@@ -11,11 +11,22 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@dictionary-lopfd.mongodb.
 mongo = PyMongo(app)
 
 
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+
+@app.route('/filter_letters/<letter>')
+def filter_letters(letter):
+    print(letter)
+
+    results = mongo.db.terms.find(
+        {"term": {"$regex": letter}})
+
+    return render_template('browseletter.html', letter=results)
+    print(term)
+
 
 @app.route('/browse')
 def browse():
@@ -34,15 +45,17 @@ def insert_word():
     terms.insert_one(request.form.to_dict())
     return redirect(url_for('browse'))
 
+
 @app.route('/delete_word/<terms_id>')
 def delete_word(terms_id):
     mongo.db.terms.remove({'_id': ObjectId(terms_id)})
     return redirect(url_for('browse'))
 
+
 @app.route('/edit_word/<terms_id>')
 def edit_word(terms_id):
-    the_term =  mongo.db.terms.find_one({"_id": ObjectId(terms_id)})
-    all_specialities =  mongo.db.speciality.find()
+    the_term = mongo.db.terms.find_one({"_id": ObjectId(terms_id)})
+    all_specialities = mongo.db.speciality.find()
     return render_template('editword.html', term=the_term,
                            speciality=all_specialities)
 
@@ -50,23 +63,25 @@ def edit_word(terms_id):
 @app.route('/update_word/<terms_id>', methods=["POST"])
 def update_word(terms_id):
     terms = mongo.db.terms
-    terms.update( {'_id': ObjectId(terms_id)},
-    {
-        'term':request.form.get('term'),
-        'definition':request.form.get('definition'),
+    terms.update({'_id': ObjectId(terms_id)},
+                 {
+        'term': request.form.get('term'),
+        'definition': request.form.get('definition'),
         'speciality_name': request.form.get('speciality_name'),
     })
     return redirect(url_for('browse'))
+
 
 @app.route('/get_specialities')
 def get_specialities():
     return render_template('specialities.html',
                            speciality=mongo.db.speciality.find())
 
+
 @app.route('/edit_speciality/<speciality_id>')
 def edit_speciality(speciality_id):
     return render_template('editspeciality.html',
-    speciality=mongo.db.speciality.find_one({'_id': ObjectId(speciality_id)}))
+                           speciality=mongo.db.speciality.find_one({'_id': ObjectId(speciality_id)}))
 
 
 @app.route('/update_speciality/<speciality_id>', methods=['POST'])
@@ -76,22 +91,23 @@ def update_speciality(speciality_id):
         {'speciality_name': request.form.get('speciality_name')})
     return redirect(url_for('get_specialities'))
 
+
 @app.route('/insert_speciality', methods=['POST'])
 def insert_speciality():
     speciality_doc = {'speciality_name': request.form.get('speciality_name')}
     mongo.db.speciality.insert_one(speciality_doc)
     return redirect(url_for('get_specialities'))    
 
+
 @app.route('/delete_speciality/<speciality_id>')
 def delete_speciality(speciality_id):
     mongo.db.speciality.remove({'_id': ObjectId(speciality_id)})
     return redirect(url_for('get_specialities'))
 
+
 @app.route('/add_speciality')
 def add_speciality():
     return render_template('addspeciality.html')
-
-
 
 
 if __name__ == '__main__':
